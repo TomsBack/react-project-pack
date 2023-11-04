e6365c1d7d52d531222b0be9f01f62a9513b07af
TomsBack <oplpet95@gmail.com>
2023-11-04 16:26:22 +0100

    init pack

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+
+export default {
+	plugins: {
+		"postcss-import": {},
+		"tailwindcss/nesting": {},
+		"postcss-preset-env": {},
+		tailwindcss: {},
+		autoprefixer: {},
+		cssnano: {},
+	},
+};
