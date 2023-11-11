0f54792f8f3fddbbe4a8d3b5f5b45757375a9f30
TomsBack <oplpet95@gmail.com>
2023-11-11 22:33:03 +0100

    mui lib testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { AddReaction } from "@mui/icons-material";
+import {
+	Accordion,
+	AccordionSummary,
+	Autocomplete,
+	Button,
+	TextField,
+	Typography,
+} from "@mui/material";
+
+const options = ["fdskj", "fdsklfj"];
+
 function App() {
-	return <div />;
+	return (
+		<div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
+			<Button variant="contained" startIcon={<AddReaction />}>
+				Button
+			</Button>
+			<Accordion className="w-1/3" elevation={10}>
+				<AccordionSummary className="block">
+					<div className="block overflow-hidden truncate">
+						<Typography className="truncate">
+							fkdlasjlkfasdjfsklajflkdsjfksadjfiosdajiofjsdaiofjsdiofjidosafjiosdjfaiosjojdiosajifojdiofdsiofjdsofjsdiofjiodsjfidsjfaisojiojfdisoafiodsjiofjdsaiofjdisofjiosdjfos
+						</Typography>
+					</div>
+				</AccordionSummary>
+			</Accordion>
+
+			<Autocomplete
+				disablePortal
+				id="combo-box-demo"
+				options={options}
+				sx={{ width: 300 }}
+				renderInput={(params) => (
+					<TextField color="success" {...params} label="Movie" />
+				)}
+			/>
+		</div>
+	);
 }
 
 export default App;
