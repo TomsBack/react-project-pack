e5664e8b17635e3ba767ceb87ee88260edfd0ce7
TomsBack <oplpet95@gmail.com>
2023-11-11 22:47:20 +0100

    accordion cursed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { AddReaction } from "@mui/icons-material";
+import { AddReaction, ExpandMore } from "@mui/icons-material";
 import {
 	Accordion,
+	AccordionDetails,
 	AccordionSummary,
 	Autocomplete,
 	Button,
@@ -17,13 +18,35 @@ function App() {
 				Button
 			</Button>
 			<Accordion className="w-1/3" elevation={10}>
-				<AccordionSummary className="block">
-					<div className="block overflow-hidden truncate">
-						<Typography className="truncate">
-							fkdlasjlkfasdjfsklajflkdsjfksadjfiosdajiofjsdaiofjsdiofjidosafjiosdjfaiosjojdiosajifojdiofdsiofjdsofjsdiofjiodsjfidsjfaisojiojfdisoafiodsjiofjdsaiofjdisofjiosdjfos
-						</Typography>
-					</div>
+				<AccordionSummary expandIcon={<ExpandMore />}>
+					Expand Me
 				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Numquam quia repellendus maxime, magnam hic fuga et iure
+						neque deserunt minima necessitatibus harum aperiam
+						aspernatur accusamus dolorem esse vel amet? Itaque!
+						Pariatur aperiam, fugit voluptas recusandae ipsa
+						mollitia aliquid soluta architecto. Cum id dignissimos
+						ab soluta voluptatibus ipsa labore totam? Quibusdam
+						beatae architecto tempore cupiditate tenetur mollitia
+						numquam sapiente, ullam minima. Rem cum fugiat repellat
+						dolore minima similique dicta dolorum rerum, vel
+						possimus quae. Magni eos repudiandae voluptates iusto
+						recusandae aspernatur, nobis, placeat minima corporis ea
+						quibusdam aliquid cum ipsam sunt! Tempore facere quae
+						aut ex ad. Distinctio error necessitatibus aliquam amet
+						praesentium eum autem dolores nihil, deleniti
+						perspiciatis facere. Ut quis, quam est debitis adipisci
+						nesciunt ullam id doloremque magni. Nisi doloremque quod
+						consequuntur pariatur necessitatibus explicabo est
+						delectus eius quae voluptatibus, nihil accusantium.
+						Aspernatur ipsam quod reprehenderit aut pariatur sunt
+						perferendis ducimus adipisci? Commodi dolores quasi
+						repudiandae deleniti sit.
+					</Typography>
+				</AccordionDetails>
 			</Accordion>
 
 			<Autocomplete
@@ -32,7 +55,7 @@ function App() {
 				options={options}
 				sx={{ width: 300 }}
 				renderInput={(params) => (
-					<TextField color="success" {...params} label="Movie" />
+					<TextField {...params} label="Movie" />
 				)}
 			/>
 		</div>
