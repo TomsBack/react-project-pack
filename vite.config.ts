c3565fd47232efd9b53fa49a07c3948cd063be95
El <oplpet95@gmail.com>
2024-06-01 00:42:22 +0200

    type checker, classname formatter, package update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), checker({ typescript: true })],
 	server: {
 		port: 3000,
 	},
