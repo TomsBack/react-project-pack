e1c92374bdb5d391c6bffae52c5d0d122f2398d3
El_Tomlino <oplpet95@gmail.com>
2025-05-31 22:05:34 +0200

    Update ESLint configuration to ignore additional config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ import tseslint from "typescript-eslint";
 
 export default defineConfig([
 	{
-		ignores: ["eslint.config.mjs"],
+		ignores: ["eslint.config.mjs", "tailwind.config.ts", "vite.config.ts"],
 	},
 	js.configs.recommended,
 	eslintConfigPrettier,
