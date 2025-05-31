aa9b779e7d492d2b6a883edb2b47bce34d24b57a
El_Tomlino <oplpet95@gmail.com>
2025-05-31 22:02:43 +0200

    Refactor eslint to use the flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,26 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import react from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import reactPlugin from "eslint-plugin-react";
+import * as reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
 	{
-		extends: fixupConfigRules(
-			compat.extends(
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:react-hooks/recommended",
-				"plugin:react/jsx-runtime",
-				"prettier"
-			)
-		),
-
-		plugins: {
-			"@typescript-eslint": fixupPluginRules(typescriptEslint),
-			react: fixupPluginRules(react),
-			"react-refresh": reactRefresh,
-		},
-
+		ignores: ["eslint.config.mjs"],
+	},
+	js.configs.recommended,
+	eslintConfigPrettier,
+	tseslint.configs.recommendedTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	reactPlugin.configs.flat.recommended,
+	reactPlugin.configs.flat["jsx-runtime"],
+	reactRefresh.configs.recommended,
+	reactHooks.configs.recommended,
+	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
@@ -49,6 +34,8 @@ export default defineConfig([
 				ecmaFeatures: {
 					jsx: true,
 				},
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
 			},
 		},
 
@@ -63,11 +50,9 @@ export default defineConfig([
 					markers: ["/"],
 				},
 			],
-
 			quotes: ["warn", "double"],
 			"no-duplicate-imports": "error",
 			"react/jsx-fragments": "error",
-
 			"react/self-closing-comp": [
 				"error",
 				{
@@ -83,8 +68,14 @@ export default defineConfig([
 				},
 			],
 
-			"no-unused-vars": "off",
-			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-misused-promises": [
+				"error",
+				{
+					checksSpreads: false,
+				},
+			],
+			"@typescript-eslint/no-misused-promises": ["off"],
 		},
 	},
+	{ settings: { react: { version: "19" } } },
 ]);
